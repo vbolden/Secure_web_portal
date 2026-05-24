7a441d4dba0bcb74a6e37ecb594214c634764031
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-24 13:39:25 -0400

    added password hashing and compare

--- models/User.js
+++ models/User.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String },
     githubId: { type: String }
 });
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password") || this.isNew) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
