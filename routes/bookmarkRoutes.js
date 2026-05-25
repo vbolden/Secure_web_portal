bff26be5ab031191232be4cc318bc764de7916f7
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-25 09:30:52 -0400

    created a post route for bookmark

---
+++ routes/bookmarkRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const Bookmark = require('../models/Bookmark');
+
+// CREATE
+router.post("/", async (req, res) => {
+    try {
+        const newBookmark = await Bookmark.create({
+            ...req.body,
+            user: req.user._id
+        });
+        res.status(201).json(newBookmark);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// READ ALL
+
+// READ ONE
+
+// UPDATE
+
+// DELETE
